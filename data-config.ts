520e7eae7e8f7afbb13a003a444cbb59b9645c52
Daniel Bolivar <daniel.bolivar.freelance@gmail.com>
2021-07-27 00:08:03 -0500

    cambios rutas data-config - Daniel bolivar - 26-07-2021

--- data-config.ts
+++ data-config.ts
@@ -2,17 +2,17 @@
  * @author Daniel Bolivar - debb94 github - daniel.bolivar.freelancer@gmail.com
  * @description Archivo con variables de configuracion de la aplicacion.
  * @version 1.0.0
- * @since 22-05-2020
+ * @since 26-06-2021
  */
 export const config = {
     // development
-    urlKaysenBackend: 'http://localhost:8080',
+    // urlKaysenBackend: 'http://localhost:8080',
     // urlKaysenFrontend: 'http://localhost:4200',
     // test
     // urlKaysenBackend: '',
     // urlKaysenFrontend: '',
     // production   
-    // urlKaysenBackend:   'https://pruebadesarrollosofka.herokuapp.com',
-    // urlKaysenFrontend:  '',
+    urlKaysenBackend:   'https://pruebadesarrollosofka.herokuapp.com',
+    urlKaysenFrontend:  'http://3.13.186.10',
 
 }
